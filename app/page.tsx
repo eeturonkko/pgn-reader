babbe9bce8b0aa12263158f29284adad01a43115
Eetu Rönkkö <eetu.kristian.ronkko@gmail.com>
2025-03-24 16:32:34 +0200

    init

--- app/page.tsx
+++ app/page.tsx
@@ -1,103 +1,151 @@
-import Image from "next/image";
+"use client";
+import { Chess } from "chess.js";
+import { useState } from "react";
+import { Chessboard } from "react-chessboard";
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+  const [moves, setMoves] = useState<string[]>([]);
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
+  return (
+    <>
+      <PGNBoard moves={moves} />
+      <PGNReader setMoves={setMoves} />
+    </>
+  );
+}
+
+export function PGNReader({
+  setMoves,
+}: {
+  setMoves: (moves: string[]) => void;
+}) {
+  const [pgn, setPgn] = useState("");
+  const [toggle, setToggle] = useState(false);
+  const [error, setError] = useState("");
+  const [showStatus, setShowStatus] = useState("Hide");
+
+  const handlePGNInput = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+    const inputPGN = event.target.value;
+    setPgn(inputPGN);
+
+    const chess = new Chess();
+    try {
+      chess.loadPgn(inputPGN);
+      setMoves(chess.history());
+      setError("");
+    } catch (error) {
+      console.log("Invalid PGN:", error);
+      setError("Invalid PGN");
+      setMoves([]);
+    }
+  };
+
+  const handleToggle = () => {
+    setToggle(!toggle);
+    setShowStatus(toggle ? "Hide" : "Show");
+  };
+
+  return (
+    <div className="container mx-auto max-w-xl text-center">
+      <div className="flex items-center justify-between">
+        <button
+          className="my-5  bg-amber-700 rounded py-2 px-4 font-bold"
+          onClick={handleToggle}
         >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
+          {showStatus} PGN input
+        </button>
+        {error && <p className="text-red-500">{error}</p>}
+      </div>
+
+      <textarea
+        className={`bg-gray-300 p-2 w-full text-black ${
+          toggle ? "hidden" : ""
+        }`}
+        value={pgn}
+        onChange={handlePGNInput}
+        placeholder="Paste PGN here..."
+        rows={5}
+      />
+    </div>
+  );
+}
+
+export function PGNBoard({ moves }: { moves: string[] }) {
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [game, setGame] = useState(new Chess());
+  const [orientation, setOrientation] = useState<"white" | "black">("white");
+
+  const applyMovesUpTo = (index: number) => {
+    const newGame = new Chess();
+    for (let i = 0; i < index; i++) {
+      newGame.move(moves[i]);
+    }
+    setGame(newGame);
+  };
+
+  const handleNext = () => {
+    if (currentIndex < moves.length) {
+      const newIndex = currentIndex + 1;
+      setCurrentIndex(newIndex);
+      applyMovesUpTo(newIndex);
+    }
+  };
+
+  const handleBack = () => {
+    if (currentIndex > 0) {
+      const newIndex = currentIndex - 1;
+      setCurrentIndex(newIndex);
+      applyMovesUpTo(newIndex);
+    }
+  };
+
+  const handleReset = () => {
+    setCurrentIndex(0);
+    setGame(new Chess());
+  };
+
+  const handleBoardFlip = () => {
+    setOrientation(orientation === "white" ? "black" : "white");
+  };
+
+  return (
+    <div className="container max-w-xl mx-auto text-center">
+      <h1 className="text-6xl font-bold mt-6">Chess PGN Reader</h1>
+      <div className="flex items-center justify-between">
+        <button
+          className="my-5  bg-amber-700 rounded py-2 px-4 font-bold"
+          onClick={handleReset}
         >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
+          Reset
+        </button>
+        <button
+          className="my-5  bg-amber-700 rounded py-2 px-4 font-bold"
+          onClick={handleBoardFlip}
         >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+          Flip board
+        </button>
+      </div>
+
+      <Chessboard position={game.fen()} boardOrientation={orientation} />
+
+      <div className="flex justify-evenly">
+        <button
+          onClick={handleBack}
+          className="my-5 bg-amber-700 rounded py-2 px-4 font-bold"
+          disabled={currentIndex === 0}
+        >
+          Back
+        </button>
+        <button
+          onClick={handleNext}
+          className={`my-5 bg-amber-700 rounded py-2 px-4 font-bold ${
+            currentIndex === moves.length ? "opacity-50" : ""
+          }`}
+          disabled={currentIndex === moves.length}
+        >
+          Next
+        </button>
+      </div>
     </div>
   );
 }
